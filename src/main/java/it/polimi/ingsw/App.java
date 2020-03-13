8941a0cccb5a87d0ba6e76a38af3ca9f4668d221
simone sarti <simonesarti98@gmail.com>
2020-03-13 21:15:29 +0100

    eliminazione righe test, creazione package per MVC

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -9,9 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "test modifica" );
-        System.out.println("test3: modificato da Alessandro");
-        //Olimpia rispondi al commento se ti funziona:
-        //eccomi arrivata. é stato facile
+
     }
 }
