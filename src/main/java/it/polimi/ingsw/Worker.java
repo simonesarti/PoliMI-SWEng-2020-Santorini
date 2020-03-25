ad9dd978df6d470a8c6fd4cfaa116bb71bb5a18f
simone sarti <simonesarti98@gmail.com>
2020-03-25 17:44:23 +0100

    creazione classe Worker con costruttore personalizzato con posizione iniziale

---
+++ src/main/java/it/polimi/ingsw/Worker.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw;
+
+public class Worker {
+
+    private Position previousPosition;
+    private Position currentPosition;
+
+    public Worker(Position startingPosition) {
+        this.previousPosition=null;
+        this.currentPosition=startingPosition;
+    }
+
+
+
+}
