85478684526c488801816a23aba0e19639766c18
simone sarti <simonesarti98@gmail.com>
2020-03-25 18:32:16 +0100

    aggiunto metodo isEnemy all'enum Colour

--- src/main/java/it/polimi/ingsw/Colour.java
+++ src/main/java/it/polimi/ingsw/Colour.java
@@ -2,5 +2,15 @@ package it.polimi.ingsw;
 
 public enum Colour {
 
-    WHITE,BLUE,GREY;
+    WHITE, BLUE, GREY;
+
+    public boolean isEnemy(Colour other) {
+
+        if (this == other) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
 }
