c9a2bdef8bcd37140814a46fd93da6ce10c8afb0
simone sarti <simonesarti98@gmail.com>
2020-03-27 19:17:31 +0100

    creata la struttura per la classe view

---
+++ src/main/java/it/polimi/ingsw/view/View.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.view;
+
+import java.io.PrintStream;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+
+
+public class View extends Observable implements Runnable, Observer {
+
+    //va bene solo per scrittura su CLI
+    private Scanner scanner;
+    private PrintStream outputStream;
+
+    //stessa cosa
+    public View(){
+        scanner = new Scanner(System.in);
+        outputStream = new PrintStream(System.out);
+    }
+
+
+    @Override
+    public void run(){
+
+        System.out.println("Please insert your nickname: ");
+
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
