f8d0e596a0d3d342389b5ae2697d90855a9e34fd
simone sarti <simonesarti98@gmail.com>
2020-03-29 13:02:03 +0200

    spostate classi nel model, creata classe astratta Piece

--- src/main/java/it/polimi/ingsw/Block.java
+++
@@ -1,8 +0,0 @@
-package it.polimi.ingsw;
-
-public class Block {
-
-    public Block() {
-
-    }
-}
--- src/main/java/it/polimi/ingsw/model/Level3Block.java
+++ src/main/java/it/polimi/ingsw/model/Level3Block.java
@@ -1,4 +1,4 @@
-package it.polimi.ingsw;
+package it.polimi.ingsw.model;
 
 public class Level3Block extends Block {
 
