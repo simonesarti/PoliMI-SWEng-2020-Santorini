9485e58956abb531376b6d3dcc11307f764e1302
olimpiarivera <olimpia.rivera@mail.polimi.it>
2020-04-03 23:08:21 +0200

    aggiunta codice nella BuildStrategy

--- src/main/java/it/polimi/ingsw/model/strategy/buildstrategy/BasicBuild.java
+++ src/main/java/it/polimi/ingsw/model/strategy/buildstrategy/BasicBuild.java
@@ -1,4 +1,13 @@
 package it.polimi.ingsw.model.strategy.buildstrategy;
 
+import it.polimi.ingsw.model.GameBoard;
+import it.polimi.ingsw.model.Position;
+import it.polimi.ingsw.model.Worker;
+
 public class BasicBuild implements BuildStrategy {
+
+    @Override
+    public void build(GameBoard gameboard, Worker worker, int x, int y) {
+
+    }
 }
--- src/main/java/it/polimi/ingsw/model/strategy/buildstrategy/BuildStrategy.java
+++ src/main/java/it/polimi/ingsw/model/strategy/buildstrategy/BuildStrategy.java
@@ -1,4 +1,15 @@
 package it.polimi.ingsw.model.strategy.buildstrategy;
 
+import it.polimi.ingsw.model.GameBoard;
+import it.polimi.ingsw.model.Worker;
+
 public interface BuildStrategy {
+
+    /**
+     * @param gameboard
+     * @param worker worker scelto per il turno
+     * @param x x della towerCell in cui il player ha deciso di costruire
+     * @param y y della towerCell in cui il player ha deciso di costruire
+     */
+    void build(GameBoard gameboard, Worker worker, int x, int y);
 }
