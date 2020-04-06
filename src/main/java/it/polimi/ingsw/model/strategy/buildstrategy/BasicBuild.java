b3fe8421ec47f5dc5e2a5535d3a36d64177e5a28
olimpiarivera <olimpia.rivera@mail.polimi.it>
2020-04-06 22:23:15 +0200

    Modifica BasicBuild passando come parametro il messaggio

--- src/main/java/it/polimi/ingsw/model/strategy/buildstrategy/BasicBuild.java
+++ src/main/java/it/polimi/ingsw/model/strategy/buildstrategy/BasicBuild.java
@@ -1,10 +1,12 @@
 package it.polimi.ingsw.model.strategy.buildstrategy;
 
+import it.polimi.ingsw.messages.PlayerMovementChoice;
 import it.polimi.ingsw.model.GameBoard;
 import it.polimi.ingsw.model.Position;
 import it.polimi.ingsw.model.Worker;
 import it.polimi.ingsw.model.piece.Block;
 import it.polimi.ingsw.model.piece.Piece;
+import it.polimi.ingsw.messages.PlayerBuildChoice;
 
 
 public class BasicBuild implements BuildStrategy {
@@ -14,17 +16,20 @@ public class BasicBuild implements BuildStrategy {
      * changes towercell's height and checks if towercell is complete
      *
      * @param gameboard
-     * @param worker chosen worker
-     * @param x destination-towercell's x
-     * @param y destination-towercell's y
+     * @param message messaggio PlayerBuildChoice
      */
     @Override
-    public void build(GameBoard gameboard, Worker worker, Piece piece, int x, int y) {
+    public void build(GameBoard gameboard, PlayerBuildChoice message) {
 
-        Position buildingPosition = new Position(x, y, gameboard.getTowerCell(x, y).getTowerHeight()+1);
+        Worker worker = message.getPlayer().getWorker(message.getChosenWorker());
+        int x = message.getBuildingInto()[0];
+        int y = message.getBuildingInto()[1];
+        Position buildingPosition = new Position(x, y, gameboard.getTowerCell(x, y).getTowerHeight());
         Position workerStartingPosition = new Position(worker.getCurrentPosition().getX(), worker.getCurrentPosition().getY(),worker.getCurrentPosition().getZ());
 
-        //controllo che x e y siano all'interno della build
+
+
+        //controllo che x e y siano all'interno della board
         if (x < 0 || x > 4 || y < 0 || y > 4) {
 
             //TODO notify() -> spedire messaggio errore alla view
@@ -45,13 +50,20 @@ public class BasicBuild implements BuildStrategy {
         }
 
         //se il pezzo che il giocatore ha scelto è di tipo Block e la torre ha già altezza 3, mando mex di errore
-        else if(gameboard.getTowerCell(x,y).getTowerHeight()==3 &&  piece instanceof Block){
+        else if(gameboard.getTowerCell(x,y).getTowerHeight()==3 &&  message.getPieceType().equals("Block")){
             //TODO notify() -> spedire messaggio errore alla view
             return;
         }
 
+        //se il pezzo che il giocatore ha scelto è di tipo Dome e la torre ha altezza < 3, mando mex di errore
+        else if(gameboard.getTowerCell(x,y).getTowerHeight()<3 &&  message.getPieceType().equals("Dome")){
+            //TODO notify() -> spedire messaggio errore alla view
+            return;
+        }
+
+
         else {
-            //TODO gameboard.getTowerCell(x, y).getTowerHeight()== vecchia TowerHeight +1
+            gameboard.getTowerCell(x, y).increaseTowerHeight();
 
             //se il piece scelto era di tipo dome, chiamo il metodo checkCompletion() che renderà true l'attributo towerCompleted.
             gameboard.getTowerCell(x,y).checkCompletion();
--- src/main/java/it/polimi/ingsw/model/strategy/buildstrategy/BuildStrategy.java
+++ src/main/java/it/polimi/ingsw/model/strategy/buildstrategy/BuildStrategy.java
@@ -1,17 +1,17 @@
 package it.polimi.ingsw.model.strategy.buildstrategy;
 
+import it.polimi.ingsw.messages.PlayerBuildChoice;
 import it.polimi.ingsw.model.GameBoard;
 import it.polimi.ingsw.model.Worker;
 import it.polimi.ingsw.model.Position;
 import it.polimi.ingsw.model.piece.Piece;
+import it.polimi.ingsw.messages.PlayerBuildChoice;
 
 public interface BuildStrategy {
 
     /**
      * @param gameboard
-     * @param worker chosen worker
-     * @param x destination-towercell's x
-     * @param y destination-towercell's y
+     *
      */
-    void build(GameBoard gameboard, Worker worker, Piece piece, int x, int y);
+    public void build(GameBoard gameboard, PlayerBuildChoice message);
 }
