64aa78707a5a1cce355a197846b64897a6ccc4b3
simone sarti <simonesarti98@gmail.com>
2020-04-07 17:19:22 +0200

    coomit test

---
+++ src/test/java/it/polimi/ingsw/model/WorkerTest.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkerTest {
+
+    Worker worker;
+
+    @BeforeEach
+    void init() {
+        worker = new Worker(Colour.WHITE);
+        worker.setStartingPosition(1, 2);
+    }
+
+    @Test
+    @DisplayName("setStartingPositionTest")
+    void setStartingPosition() {
+
+        assertAll(
+                () -> assertEquals(worker.getCurrentPosition().getX(), 1),
+                () -> assertEquals(worker.getCurrentPosition().getY(), 2),
+                () -> assertEquals(worker.getCurrentPosition().getZ(), 0),
+                () -> assertNull(worker.getPreviousPosition())
+        );
+    }
+
+    @Test
+    @DisplayName("movedToPositionTest")
+    void movedToPosition() {
+        worker.movedToPosition(3, 4, 5);
+
+        assertAll(
+                () -> assertEquals(worker.getCurrentPosition().getX(), 3),
+                () -> assertEquals(worker.getCurrentPosition().getY(), 4),
+                () -> assertEquals(worker.getCurrentPosition().getZ(), 5),
+                () -> assertEquals(worker.getPreviousPosition().getX(), 1),
+                () -> assertEquals(worker.getPreviousPosition().getY(), 2),
+                () -> assertEquals(worker.getPreviousPosition().getZ(), 0)
+        );
+    }
+}
