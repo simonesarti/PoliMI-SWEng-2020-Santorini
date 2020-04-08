1fa064f5c086e4fd153eacc8552b24ac1bb06b11
simone sarti <simonesarti98@gmail.com>
2020-04-08 14:06:36 +0200

    aggiunta Classe GameMessage

---
+++ src/main/java/it/polimi/ingsw/messages/GameMessage.java
@@ -0,0 +1,36 @@
+package it.polimi.ingsw.messages;
+
+public class GameMessage {
+
+    //Generic move error messages
+    public static String noMoveToCompleteTower = "You can't move here, this space already contains a complete tower, please choose another position\n";
+    public static String noMovedToOccupiedTower = "You can't move here, this space already contains another worker, please choose another position\n";
+    public static String noHighJump = "This move isn't allowed, you can't move up more than one level. Choose another position\n";
+
+    //move ok
+    public static String moveOK= "ok";
+
+    //generic build error messages
+    public static String noBuildToCompleteTower = "You can't build here, this space already contains a complete tower, please choose another position\n";
+    public static String noBuildToOccupiedTower = "You can't build here, this space already contains a worker, please choose another position\n";
+    public static String noLevel1Left = "No more first level blocks left. Please try something else\n";
+    public static String noLevel2Left = "No more second level blocks left. Please try something else\n";
+    public static String noLevel3Left = "No more third level blocks left. Please try something else\n";
+    public static String noDomesLeft = "No more domes left. Please try something else\n";
+    public static String noDomesInBlock = "You can't place a dome in a space reserved to a block. Please try something else\n";
+    public static String noBlocksInDome = "You can't place a block in a space reserved to a dome. Please try something else\n";
+
+    //build ok
+    public static String buildOK= "ok";
+
+    //move AND build error messages
+    public static String notInSurroundings = "This move is not allowed as it is not to one of the cell surrounding the selected worker\n";
+    public static String notInGameboard = "This position in outside the gameboard, try again \n";
+
+    //GOD SPECIFIC MESSAGES
+
+    //ATHENA
+    public static String athenaNoMoveUp = "This move isn't allowed due to Athena's power";
+
+
+}
