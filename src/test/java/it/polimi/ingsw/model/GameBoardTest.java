c3d1626a90029bf2229b801bb35c838e6edf1c68
simone sarti <simonesarti98@gmail.com>
2020-04-15 14:55:47 +0200

    test spinta

--- src/test/java/it/polimi/ingsw/model/GameBoardTest.java
+++ src/test/java/it/polimi/ingsw/model/GameBoardTest.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.messages.PlayerInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -52,6 +56,42 @@ class GameBoardTest {
         assertEquals(3,gameBoard.getTowerCell(2,0).getTowerHeight());
         assertEquals(4,gameBoard.getTowerCell(3,0).getTowerHeight());
         assertEquals(0,gameBoard.getTowerCell(4,0).getTowerHeight());
+
+    }
+
+    @Test
+    void cantBeForcedBackwards() {
+        int[][] towers=
+                {
+                        {1,2,3,4,0},
+                        {0,1,2,4,4},
+                        {3,2,0,1,0},
+                        {1,3,2,2,1},
+                        {0,0,0,0,0}
+                };
+        gameBoard.generateBoard(towers);
+
+        //out of gameboard
+        assertTrue(gameBoard.cantBeForcedBackwards(1,1,0,0));
+        //out of gameboard
+        assertTrue(gameBoard.cantBeForcedBackwards(1,1,1,0));
+        //out of gameboard
+        assertTrue(gameBoard.cantBeForcedBackwards(1,1,2,0));
+        //completed tower behind
+        assertTrue(gameBoard.cantBeForcedBackwards(1,1,2,1));
+
+        PlayerInfo playerInfo=new PlayerInfo("enemy1",new GregorianCalendar(2000, Calendar.NOVEMBER, 30));
+        Player enemy=new Player(playerInfo);
+        enemy.setColour(Colour.GREY);
+        enemy.getWorker(0).setStartingPosition(0,0);
+        gameBoard.getTowerCell(3,3).getFirstNotPieceLevel().setWorker(enemy.getWorker(0));
+        enemy.getWorker(0).movedToPosition(3,3,2);
+
+        //worker behind
+        assertTrue(gameBoard.cantBeForcedBackwards(1,1,2,2));
+
+        //can push
+        assertFalse(gameBoard.cantBeForcedBackwards(1,1,1,2));
 
     }
 }
