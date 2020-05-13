94a47414e1487dfdfbbdddd45fbfe6ccc302b1ba
olimpiarivera <olimpia.rivera@mail.polimi.it>
2020-05-14 01:37:48 +0200

    modificata classe Cell della CLI

--- src/main/java/it/polimi/ingsw/view/cli/Cell.java
+++ src/main/java/it/polimi/ingsw/view/cli/Cell.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.view.cli;
 
 
 
+
 public class Cell {
 
     private String[] stripes = new String[4];
@@ -13,148 +14,38 @@ public class Cell {
 
     }
 
-    public void FirstEmptyCell(){
-        stripes[0] = "\u001B[42m            \u001B[0m";
-        stripes[1] = "\u001B[42m            \u001B[0m";
-        stripes[2] = "\u001B[42m            \u001B[0m";
-        stripes[3] = "\u001B[42m            \u001B[0m";
-        stripes[4] = "\u001B[42m            \u001B[0m";
-    }
-
-    public void FirstCellWithWorker1(){
-        stripes[0] = "\u001B[42m            \u001B[0m";
-        stripes[1] = "\u001B[42m            \u001B[0m";
-        stripes[2] = "\u001B[42m     \u001B[30mW1     \u001B[0m";
-        stripes[3] = "\u001B[42m            \u001B[0m";
-        stripes[4] = "\u001B[42m            \u001B[0m";
-    }
-
-    public void FirstCellWithWorker2(){
-        stripes[0] = "\u001B[42m            \u001B[0m";
-        stripes[1] = "\u001B[42m            \u001B[0m";
-        stripes[2] = "\u001B[42m     \u001B[30mW2     \u001B[0m";
-        stripes[3] = "\u001B[42m            \u001B[0m";
-        stripes[4] = "\u001B[42m            \u001B[0m";
-    }
 
 
-    public void Level1Cell (){
-        stripes[0] = "\u001B[47m            \u001B[0m";
-        stripes[1] = "\u001B[47m            \u001B[0m";
-        stripes[2] = "\u001B[47m    \u001B[30mLV 1    \u001B[0m";
-        stripes[3] = "\u001B[47m            \u001B[0m";
-        stripes[4] = "\u001B[47m            \u001B[0m";
-    }
-
-    public void Level2Cell (){
-        stripes[0] = "\u001B[47m            \u001B[0m";
-        stripes[1] = "\u001B[47m            \u001B[0m";
-        stripes[2] = "\u001B[47m    \u001B[30mLV 2    \u001B[0m";
-        stripes[3] = "\u001B[47m            \u001B[0m";
-        stripes[4] = "\u001B[47m            \u001B[0m";
-    }
-
-    public void Level3Cell (){
-        stripes[0] = "\u001B[47m            \u001B[0m";
-        stripes[1] = "\u001B[47m            \u001B[0m";
-        stripes[2] = "\u001B[47m    \u001B[30mLV 3    \u001B[0m";
-        stripes[3] = "\u001B[47m            \u001B[0m";
-        stripes[4] = "\u001B[47m            \u001B[0m";
-    }
-
-    public void Level1CellWithWorker1 (){
-        stripes[0] = "\u001B[47m            \u001B[0m";
-        stripes[1] = "\u001B[47m    \u001B[30mLV 1    \u001B[0m";
-        stripes[2] = "\u001B[47m            \u001B[0m";
-        stripes[3] = "\u001B[47m     \u001B[30mW1     \u001B[0m";
-        stripes[4] = "\u001B[47m            \u001B[0m";
-    }
-
-    public void Level1CellWithWorker2 (){
-        stripes[0] = "\u001B[47m            \u001B[0m";
-        stripes[1] = "\u001B[47m    \u001B[30mLV 1    \u001B[0m";
-        stripes[2] = "\u001B[47m            \u001B[0m";
-        stripes[3] = "\u001B[47m     \u001B[30mW2     \u001B[0m";
-        stripes[4] = "\u001B[47m            \u001B[0m";
-    }
-
-    public void Level2CellWithWorker1 (){
-        stripes[0] = "\u001B[47m            \u001B[0m";
-        stripes[1] = "\u001B[47m    \u001B[30mLV 2    \u001B[0m";
-        stripes[2] = "\u001B[47m            \u001B[0m";
-        stripes[3] = "\u001B[47m     \u001B[30mW1     \u001B[0m";
-        stripes[4] = "\u001B[47m            \u001B[0m";
-    }
-
-    public void Level2CellWithWorker2 (){
-        stripes[0] = "\u001B[47m            \u001B[0m";
-        stripes[1] = "\u001B[47m    \u001B[30mLV 2    \u001B[0m";
-        stripes[2] = "\u001B[47m            \u001B[0m";
-        stripes[3] = "\u001B[47m     \u001B[30mW2     \u001B[0m";
-        stripes[4] = "\u001B[47m            \u001B[0m";
-    }
-
-    public void Level3CellWithWorker1 (){
-        stripes[0] = "\u001B[47m            \u001B[0m";
-        stripes[1] = "\u001B[47m    \u001B[30mLV 3    \u001B[0m";
-        stripes[2] = "\u001B[47m            \u001B[0m";
-        stripes[3] = "\u001B[47m     \u001B[30mW1     \u001B[0m";
-        stripes[4] = "\u001B[47m            \u001B[0m";
-    }
-
-    public void Level3CellWithWorker2 (){
-        stripes[0] = "\u001B[47m            \u001B[0m";
-        stripes[1] = "\u001B[47m    \u001B[30mLV 3    \u001B[0m";
-        stripes[2] = "\u001B[47m            \u001B[0m";
-        stripes[3] = "\u001B[47m     \u001B[30mW2     \u001B[0m";
-        stripes[4] = "\u001B[47m            \u001B[0m";
-    }
-
-    public void CellWithDome(){
-        stripes[0] = "\u001B[47m            \u001B[0m";
-        stripes[1] = "\u001B[47m  \u001B[44m        \u001B[47m  \u001B[0m";
-        stripes[2] = "\u001B[47m  \u001B[44m        \u001B[47m  \u001B[0m";
-        stripes[3] = "\u001B[47m  \u001B[44m        \u001B[47m  \u001B[0m";
-        stripes[4] = "\u001B[47m            \u001B[0m";
-    }
-
-    public void FirstCellWithDome(){
-        stripes[0] = "\u001B[42m            \u001B[0m";
-        stripes[1] = "\u001B[42m  \u001B[44m        \u001B[42m  \u001B[0m";
-        stripes[2] = "\u001B[42m  \u001B[44m        \u001B[42m  \u001B[0m";
-        stripes[3] = "\u001B[42m  \u001B[44m        \u001B[42m  \u001B[0m";
-        stripes[4] = "\u001B[42m            \u001B[0m";
-    }
-
-
-
-    private String uniformStripe(String BackgroundAnsiColour){
-        String stripe=...;
+    public String uniformStripe(String BackgroundAnsiColour){
+        String stripe= BackgroundAnsiColour+"            "+AnsiCode.RESET;
         return stripe;
     }
 
-    private String domeStripe(String BackgroundAnsiColour){
-        String stripe=...;
+    private String domeStripe(String BackgroundAnsiColour1, String BackgroundAnsiColour2 ){
+        String stripe= BackgroundAnsiColour1+"  "+BackgroundAnsiColour2+"        "+BackgroundAnsiColour1+"  "+AnsiCode.RESET;
         return stripe;
     }
 
+    //TODO: riga con coordinata
     private String coordinateStripe(int value){
-        String stripe=...;
-        //in cui ha i ...+value.toString()+....
-        //scegli tu colore backgruond di questa
-        return stripe;
+       String stripe = "";
+       return stripe;
     }
 
     private String workerStripe(String BackgroundAnsiColour,int workerNumber){
-        //concetto di prima con coordinata, ci infili il numero come stringa dentro l'ansi+ il W o W: o come vuoi tu
+        String num = String.valueOf(workerNumber);
+        String stripe = BackgroundAnsiColour+"     "+AnsiCode.TEXT_BLACK+"W"+num+"     "+AnsiCode.RESET;
+        return stripe;
     }
 
-    private String levelStripe(int workerNumber){
-        //concetto di prima con coordinata, ci infili il numero come stringa dentro l'ansi+ il W o W: o come vuoi tu
+    private String levelStripe(int level){
+        String num = String.valueOf(level);
+        String stripe=AnsiCode.BACKGROUND_WHITE+"    "+AnsiCode.TEXT_BLACK+"LV "+num+"    "+AnsiCode.RESET;
+        return stripe;
+    }
 
-        //2 possibilità: indichiamo sempre il livello, in quel caso ci sia aggiunge anche il colore di sfondo
-        //perchè scriveremmo lv0 anche nella cella vuota e dobbiamo distingue verde/grigio
-        //oppure lasciarlo così e dire che se chiamo levelStrpe è perchè c'è almeno un livello => colore=grigio
+    public String getStripe(int n){
+        return stripes[n];
     }
 
 
--- src/main/java/it/polimi/ingsw/view/cli/Matrix.java
+++ src/main/java/it/polimi/ingsw/view/cli/Matrix.java
@@ -1,15 +1,35 @@
 package it.polimi.ingsw.view.cli;
 
+import it.polimi.ingsw.messages.GameToPlayerMessages.Notify.NewBoardStateMessage;
+
 public class Matrix {
 
-    private Cell[][] matrix = new Cell[6][6];
+    private Cell[][] board = new Cell[6][6];
 
     public Matrix(){
         for (int i=0; i<6; i++){
             for (int j=0; j<6; j++){
-                matrix[i][j]= new Cell();
+                board[i][j]= new Cell();
             }
-
         }
+    }
+
+    public Matrix convertToMatrix(NewBoardStateMessage message){
+
+        Matrix matrix = new Matrix();
+        Cell cell = new Cell();
+
+        for (int i=1; i<6; i++){
+            for (int j=1; j<6; j++){
+                if (message.getBoardState().getTowerState(i,j).getTowerHeight()==0 && message.getBoardState().getTowerState(i,j).getWorkerColour().equals(null)){
+
+
+
+                }
+            }
+        }
+
+        return matrix;
+
     }
 }
