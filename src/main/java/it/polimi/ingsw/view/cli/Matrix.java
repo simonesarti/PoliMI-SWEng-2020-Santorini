ee987ff113990b6bd3e1610e35fa29413b1de547
olimpiarivera <olimpia.rivera@mail.polimi.it>
2020-05-15 12:50:52 +0200

    Aggiunto metodo printMatrix

--- src/main/java/it/polimi/ingsw/view/cli/Matrix.java
+++ src/main/java/it/polimi/ingsw/view/cli/Matrix.java
@@ -42,24 +42,24 @@ public class Matrix {
             for (int j=1; j<6; j++){
 
                 //LEVEL 0
-                if (message.getBoardState().getTowerState(i,j).getTowerHeight()==0){
+                if (message.getBoardState().getTowerState(i-1,j-1).getTowerHeight()==0){
 
                     //no worker
-                    if(message.getBoardState().getTowerState(i, j).getWorkerColour() == null) {
+                    if(message.getBoardState().getTowerState(i-1, j-1).getWorkerColour() == null) {
                         matrix.getCell(i, j).assignGreen();
                     }
                     //with worker
                     else{
-                        matrix.getCell(i,j).assignOnlyWorker(message.getBoardState().getTowerState(i,j).getWorkerNumber());
+                        matrix.getCell(i,j).assignOnlyWorker(message.getBoardState().getTowerState(i-1,j-1).getWorkerNumber());
                     }
 
                 }
 
                 //DOME
-                else if(message.getBoardState().getTowerState(i, j).isCompleted()){
+                else if(message.getBoardState().getTowerState(i-1, j-1).isCompleted()){
 
                     //level 0
-                    if(message.getBoardState().getTowerState(i,j).getTowerHeight()==0){
+                    if(message.getBoardState().getTowerState(i-1,j-1).getTowerHeight()==0){
                         matrix.getCell(i,j).assignDome(AnsiCode.BACKGROUND_GREEN);
                     }
                     //level 1/2/3
@@ -70,14 +70,14 @@ public class Matrix {
                 }
 
                 //TOWER 1/2/3
-                else if(message.getBoardState().getTowerState(i,j).getTowerHeight()>0){
+                else if(message.getBoardState().getTowerState(i-1,j-1).getTowerHeight()>0){
                     //no worker
-                    if(message.getBoardState().getTowerState(i, j).getWorkerColour() == null){
-                        matrix.getCell(i,j).assignOnlyLevel(message.getBoardState().getTowerState(i,j).getTowerHeight());
+                    if(message.getBoardState().getTowerState(i-1, j-1).getWorkerColour() == null){
+                        matrix.getCell(i,j).assignOnlyLevel(message.getBoardState().getTowerState(i-1,j-1).getTowerHeight());
                     }
                     //with worker
                     else {
-                        matrix.getCell(i,j).assignLevelAndWorker(message.getBoardState().getTowerState(i,j).getTowerHeight(),message.getBoardState().getTowerState(i,j).getWorkerNumber());
+                        matrix.getCell(i,j).assignLevelAndWorker(message.getBoardState().getTowerState(i-1,j-1).getTowerHeight(),message.getBoardState().getTowerState(i-1,j-1).getWorkerNumber());
                     }
                 }
 
