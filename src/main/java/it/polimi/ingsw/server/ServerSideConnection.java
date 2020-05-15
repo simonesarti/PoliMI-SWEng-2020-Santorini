dc17ebf81164d053b841a676809660f48e26c958
simone sarti <simonesarti98@gmail.com>
2020-05-15 19:51:34 +0200

    rimossa dagli import noSuchElementException

--- src/main/java/it/polimi/ingsw/server/ServerSideConnection.java
+++ src/main/java/it/polimi/ingsw/server/ServerSideConnection.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.NoSuchElementException;
 
 public class ServerSideConnection extends Observable<DataMessage> implements Runnable {
 
