b50100534643ceb49739cc0e2245c900ebccfcdb
simone sarti <simonesarti98@gmail.com>
2020-06-02 19:05:41 +0200

    nuova grafica richiesta date iniziali

--- src/main/java/it/polimi/ingsw/GUI/AppGUI.java
+++ src/main/java/it/polimi/ingsw/GUI/AppGUI.java
@@ -1,13 +1,11 @@
 package it.polimi.ingsw.GUI;
 
-import it.polimi.ingsw.client.ClientSideConnection;
-
 public class AppGUI {
 
     public static void main(String[] args){
 
-        new GUI(new ClientSideConnection("127.0.0.1",12345));
-        //new PlayerInfoRequestScreen(true);
+        //new GUI(new ClientSideConnection("127.0.0.1",12345));
+        new PlayerInfoRequestScreen(false);
 
     }
 }
--- src/main/java/it/polimi/ingsw/GUI/PlayerInfoRequestScreen.java
+++ src/main/java/it/polimi/ingsw/GUI/PlayerInfoRequestScreen.java
@@ -1,20 +1,30 @@
 package it.polimi.ingsw.GUI;
 
+import it.polimi.ingsw.view.ClientViewSupportFunctions;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Calendar;
 
 
-public class PlayerInfoRequestScreen extends JDialog implements ActionListener {
+public class PlayerInfoRequestScreen extends JDialog implements ActionListener{
 
     JLabel nicknameRequest;
     JLabel invalidNickname;
     JTextField nickname;
+
     JLabel birthdayRequest;
-    JSpinner birthday;
+    JTextField day;
+    JTextField month;
+    JTextField year;
+    JTextField divider1;
+    JTextField divider2;
+    JLabel invalidBirthday;
+
     JButton confirmButton;
 
     public PlayerInfoRequestScreen(boolean isNicknameTaken){
@@ -24,56 +34,65 @@ public class PlayerInfoRequestScreen extends JDialog implements ActionListener {
         invalidNickname=new JLabel();
 
         birthdayRequest=new JLabel("Date of birth: ");
-
-        Date date=new Date();
-        birthday=new JSpinner(new SpinnerDateModel(date,null,null, Calendar.DAY_OF_MONTH));
-        JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(birthday, "dd/MM/yyyy");
-        birthday.setEditor(dateEditor);
+        day=new JTextField();
+        month=new JTextField();
+        year=new JTextField();
+        divider1=new JTextField("/");
+        divider1.setEditable(false);
+        divider2=new JTextField("/");
+        divider2.setEditable(false);
+        invalidBirthday=new JLabel();
 
 
         confirmButton=new JButton("Confirm");
+        confirmButton.addActionListener(this);
 
         if(isNicknameTaken){
             invalidNickname.setText("This username is already taken");
             invalidNickname.setForeground(Color.RED);
         }
 
-        setBounds(isNicknameTaken);
-        addToDialog();
         dialogSettings();
+        setBounds();
+        addToDialog();
+
 
     }
 
-    private void setBounds(boolean taken){
-
-        int h=0;
-        if(taken) {
-            h = 20;
-        }
+    private void setBounds(){
 
         nicknameRequest.setBounds(20,20, 100,20);
         nickname.setBounds(120,20, 200,20);
         invalidNickname.setBounds(120,40,200,20);
 
-        birthdayRequest.setBounds(20,60+h, 100,20);
-        birthday.setBounds(120,60+h, 200,20);
+        birthdayRequest.setBounds(20,70, 100,20);
+        day.setBounds(120,70,50,20);
+        divider1.setBounds(170,70,10,20);
+        month.setBounds(180,70,50,20);
+        divider2.setBounds(230,70,10,20);
+        year.setBounds(240,70,50,20);
+        invalidBirthday.setBounds(120,90,200,20);
 
-        confirmButton.setBounds(125,100+h, 150,20);
+        confirmButton.setBounds(125,130, 150,20);
     }
-
 
     private void addToDialog(){
         add(nicknameRequest);
         add(nickname);
         add(invalidNickname);
         add(birthdayRequest);
-        add(birthday);
+        add(day);
+        add(month);
+        add(year);
+        add(divider1);
+        add(divider2);
+        add(invalidBirthday);
         add(confirmButton);
     }
 
-    private void dialogSettings(){
-        setTitle("Info Request");
-        setSize(400,200);
+    private void dialogSettings() {
+        //setTitle("Info Request");
+        setSize(400, 210);
         setLayout(null);
         setVisible(true);
     }
@@ -81,5 +100,24 @@ public class PlayerInfoRequestScreen extends JDialog implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if(nickname.getText().equals("")){
+            invalidNickname.setForeground(Color.red);
+            invalidNickname.setText("You can't leave this field blank");
+        }else{
+
+            ClientViewSupportFunctions support=new ClientViewSupportFunctions();
+            if(!support.isDateValid((day.getText()+"-"+month.getText()+"-"+year.getText()),day.getText(),month.getText(),year.getText())){
+                invalidBirthday.setForeground(Color.red);
+                invalidBirthday.setText("Invalid date");
+            }else{
+                dispose();
+            }
+
+
+
+        }
+
+
     }
+
 }
