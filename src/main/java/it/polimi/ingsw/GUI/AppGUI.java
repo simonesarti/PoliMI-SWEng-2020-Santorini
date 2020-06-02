ed74aebbd5905bb6b81f9a088e57dd9fa57dca29
simone sarti <simonesarti98@gmail.com>
2020-06-02 22:00:23 +0200

    selezione grafica delle carte

--- src/main/java/it/polimi/ingsw/GUI/AppGUI.java
+++ src/main/java/it/polimi/ingsw/GUI/AppGUI.java
@@ -2,12 +2,27 @@ package it.polimi.ingsw.GUI;
 
 import it.polimi.ingsw.client.ClientSideConnection;
 
+import java.util.ArrayList;
+
 public class AppGUI {
 
     public static void main(String[] args){
 
-        new GUI(new ClientSideConnection("127.0.0.1",12345));
+        //new GUI(new ClientSideConnection("127.0.0.1",12345));
         //new PlayerInfoRequestScreen(false);
+
+        ArrayList<String> names=new ArrayList<>();
+        names.add("Apollo");
+        names.add("Artemis");
+        names.add("Athena");
+        names.add("Atlas");
+        names.add("Demeter");
+        names.add("Hephaestus");
+        names.add("Minotaur");
+        names.add("Pan");
+        names.add("Prometheus");
+        new DivinityChoiceScreen(names,3);
+
 
     }
 }
--- src/main/java/it/polimi/ingsw/GUI/DivinityChoiceScreen.java
+++ src/main/java/it/polimi/ingsw/GUI/DivinityChoiceScreen.java
@@ -1,54 +1,178 @@
 package it.polimi.ingsw.GUI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
 import java.util.ArrayList;
 
-//TODO JLIST NON CONVINCE
-public class DivinityChoiceScreen extends JDialog implements ActionListener {
+public class DivinityChoiceScreen extends JDialog{
+
+    private final ArrayList<String> selected=new ArrayList<>();
+    private final int toSelect;
+    private final int total;
+    private final GodNameLink[][] links;
 
 
-    JLabel request;
-    JList namesList;
+    public DivinityChoiceScreen(ArrayList<String> names, int n) {
 
-    public DivinityChoiceScreen(ArrayList<String> names){
-        request=new JLabel("Choose your card:");
-        DefaultListModel<String> l = new DefaultListModel<>();
-        for(String name : names){
-            l.addElement(name);
-        }
-        namesList=new JList(l);
+        toSelect=n;
+        total=names.size();
 
-        setBounds();
-        addToDialog();
-        dialogSettings();
-
-    }
-
-    private void dialogSettings(){
         setTitle("Gods");
-        setSize(300,300);
-        setLayout(null);
+        setLayout(new GridLayout(total/4+1, 4));
+        links=new GodNameLink[4][total/4+1];
+
+        for(int j=0;j<total/4+1;j++){
+            for(int i=0;i<4;i++){
+                links[i][j]=new GodNameLink();
+
+                //creates buttons
+                links[i][j].setButton(new JButton());
+
+                //associates god names
+                int index=i+4*j;
+                if(index<total){
+                    links[i][j].setName(names.get(index));
+                }else{
+                    links[i][j].setName("Empty");
+                }
+                //sets icon based on name
+                links[i][j].setButtonIcon();
+                links[i][j].getButton().setBackground(Color.GRAY);
+
+                //adds to the layout
+                add(links[i][j].getButton());
+
+                links[i][j].getButton().addActionListener(new CardSelectionListener());
+            }
+        }
+
+        pack();
         setVisible(true);
     }
 
-    private void addToDialog(){
-        add(request);
-        add(namesList);
+
+
+    private GodNameLink findLink(JButton button){
+
+        for(int j=0;j<total/4+1;j++){
+            for(int i=0;i<4;i++){
+                if(button.equals(links[i][j].getButton())){
+                    return links[i][j];
+                }
+            }
+        }
+        throw new IllegalArgumentException("link not found");
     }
 
-    private void setBounds(){
-        request.setBounds(50,50,150,20);
-        namesList.setBounds(50,100, 75,75);
+
+    private class CardSelectionListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            JButton pressed=(JButton)e.getSource();
+
+            GodNameLink link=findLink(pressed);
+
+            if(!link.getName().equals("Empty")){
+                if (selected.contains(link.getName())){
+                    selected.remove(link.getName());
+                    pressed.setBackground(Color.GRAY);
+                    //System.out.println("removed "+link.getName());
+
+                } else {
+                    pressed.setBackground(Color.CYAN);
+                    selected.add(link.getName());
+                    //System.out.println("added "+link.getName());
+                }
+                repaint();
+
+                if (selected.size() == toSelect) {
+/*
+                    for(String selected : selected){
+                        System.out.println(selected+",");
+                    }
+*/
+                    dispose();
+                }
+            }
+
+
+        }
     }
 
 
+    private class GodNameLink {
 
+        private String name;
+        private JButton button;
 
+        public String getName() {
+            return name;
+        }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+        public JButton getButton() {
+            return button;
+        }
 
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setButton(JButton button) {
+            this.button = button;
+        }
+
+        public void setButtonIcon(){
+
+            switch(name){
+
+                case "Apollo":
+                    button.setIcon(new ImageIcon(Images.getImage(Images.APOLLO).getScaledInstance(200,200,Image.SCALE_SMOOTH)));
+                    break;
+
+                case "Artemis":
+                    button.setIcon(new ImageIcon(Images.getImage(Images.ARTEMIS).getScaledInstance(200,200,Image.SCALE_SMOOTH)));
+                    break;
+
+                case "Athena":
+                    button.setIcon(new ImageIcon(Images.getImage(Images.ATHENA).getScaledInstance(200,200,Image.SCALE_SMOOTH)));
+                    break;
+
+                case "Atlas":
+                    button.setIcon(new ImageIcon(Images.getImage(Images.ATLAS).getScaledInstance(200,200,Image.SCALE_SMOOTH)));
+                    break;
+
+                case "Demeter":
+                    button.setIcon(new ImageIcon(Images.getImage(Images.DEMETER).getScaledInstance(200,200,Image.SCALE_SMOOTH)));
+                    break;
+
+                case "Hephaestus":
+                    button.setIcon(new ImageIcon(Images.getImage(Images.HEPHAESTUS).getScaledInstance(200,200,Image.SCALE_SMOOTH)));
+                    break;
+
+                case "Minotaur":
+                    button.setIcon(new ImageIcon(Images.getImage(Images.MINOTAUR).getScaledInstance(200,200,Image.SCALE_SMOOTH)));
+                    break;
+
+                case "Pan":
+                    button.setIcon(new ImageIcon(Images.getImage(Images.PAN).getScaledInstance(200,200,Image.SCALE_SMOOTH)));
+                    break;
+
+                case "Prometheus":
+                    button.setIcon(new ImageIcon(Images.getImage(Images.PROMETHEUS).getScaledInstance(200,200,Image.SCALE_SMOOTH)));
+                    break;
+
+                case "Empty":
+                    button.setIcon(new ImageIcon(Images.getImage(Images.EMPTY_CARD).getScaledInstance(200,200,Image.SCALE_SMOOTH)));
+                    break;
+
+            }
+        }
     }
+
+
 }
