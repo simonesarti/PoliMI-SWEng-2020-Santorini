871ed18b914f1ddbc072585db300bf2803aa8f1a
simone sarti <simonesarti98@gmail.com>
2020-06-03 23:06:08 +0200

    javadoc classi package observer e package player->game message (javadoc mesaggi finiti)

--- src/main/java/it/polimi/ingsw/GUI/AppGUI.java
+++ src/main/java/it/polimi/ingsw/GUI/AppGUI.java
@@ -1,12 +1,16 @@
 package it.polimi.ingsw.GUI;
 
+import it.polimi.ingsw.client.ClientSideConnection;
+
+import java.util.ArrayList;
+
 public class AppGUI {
 
     public static void main(String[] args){
 
         //new GUI(new ClientSideConnection("127.0.0.1",12345));
-        new PlayerInfoRequestScreen(false);
-/*
+        //new PlayerInfoRequestScreen(false);
+
         ArrayList<String> names=new ArrayList<>();
         names.add("Apollo");
         names.add("Artemis");
@@ -19,6 +23,6 @@ public class AppGUI {
         names.add("Prometheus");
         new DivinityChoiceScreen(names,3);
 
-*/
+
     }
 }
